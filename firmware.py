13fda2bf1428add7c69b7ab771ab43bef140eaae
Loks1601 <class10studieslokesh@gmail.com>
2025-12-23 06:18:09 +0530

    Add files via upload

---
+++ firmware.py
@@ -0,0 +1,49 @@
+import board
+from kmk.kmk_keyboard import KMKKeyboard
+from kmk.keys import KC
+from kmk.scanners.keypad import KeysScanner
+
+# ----------------------------
+# 1. KEYBOARD SETUP
+# ----------------------------
+keyboard = KMKKeyboard()
+
+# ----------------------------
+# 2. BUTTON PIN DEFINITIONS
+# ----------------------------
+# 4-button macropad
+PINS = [
+    board.D0,  # SW1
+    board.D1,  # SW2
+    board.D2,  # SW3
+    board.D3,  # SW4
+]
+
+keyboard.matrix = KeysScanner(
+    pins=PINS,
+    value_when_pressed=False,  # Active LOW
+    pull=True,                 # Enable internal pull-ups
+    interval=0.02,             # Debounce (20 ms)
+)
+
+# ----------------------------
+# 3. KEYMAP
+# ----------------------------
+# Layout:
+# [ SW1 ] [ SW2 ]
+# [ SW3 ] [ SW4 ]
+
+keyboard.keymap = [
+    [
+        KC.A,      # SW1
+        KC.B,      # SW2
+        KC.C,      # SW3
+        KC.D,      # SW4
+    ]
+]
+
+# ----------------------------
+# 4. START KEYBOARD
+# ----------------------------
+if __name__ == '__main__':
+    keyboard.go()
